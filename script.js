a70381b61bd68aa486937d203c902aa5f3d4776c
SamueleTerraroli <terraroli99@gmail.com>
2024-11-21 16:15:16 +0100

    feat: add js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const btnToggle = document.getElementById('toggle-button')
 const lightBulb = document.getElementById('bulb')
+const backgroundColor = document.getElementById('background')
+
 
 btnToggle.addEventListener('click', () =>{
-    
+  lightBulb.classList.toggle('hide');  
+  backgroundColor.classList.toggle('bg_black')
+  btnToggle.innerHTML = lightBulb.classList.contains('hide') ? 'Accendi la lampadina' : 'Spegni la lampadina';
+
+
 })
